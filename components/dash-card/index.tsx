7888519486737d848090c909258b952e974848af
Mihir2423 <mihir.elitekods@gmail.com>
2024-02-08 16:04:11 +0530

    feat: make validators page UI

--- components/dash-card/index.tsx
+++ components/dash-card/index.tsx
@@ -19,7 +19,7 @@ export const DashCards = ({
   return (
     <div className="px-4 border-r border-[#27272A]">
       <div
-        className={`col-span-1 flex flex-col gap-2 p-4 ${linear ? "bg-gradient-to-b rounded-lg from-[#1A1A1A] to-[#1A1A1A00]" : ""}`}
+        className={`col-span-1 flex flex-col gap-2 p-4 pr-0 ${linear ? "bg-gradient-to-b rounded-lg from-[#1A1A1A] to-[#1A1A1A00]" : ""}`}
       >
         <Image src={image} alt="icon" width={22} height={22} />
         <p className="text-[14px] leading-[20px] font-[500] text-[#A6A6A6] mt-1">
