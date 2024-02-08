bc0969a65753a895a96a78ec6c9821731f656b1f
techsaavy8784 <blueskydev1205@gmail.com>
2024-02-07 20:55:11 -0700

    hide some fields from admin dashboard

--- app/admin/[[...nextadmin]]/options.ts
+++ app/admin/[[...nextadmin]]/options.ts
@@ -2,6 +2,19 @@ import { NextAdminOptions } from "@premieroctet/next-admin"
 
 const options: NextAdminOptions = {
   basePath: "/admin",
+  model: {
+    Blockchain: {
+      edit: {
+        display: ["id", "name", "url", "price", "launchedAt"],
+      },
+    },
+    Server: {
+      edit: {
+        display: ["id", "ssh", "command", "active", "hasWallet", "blockchain"],
+      },
+    },
+    User: {},
+  },
 }
 
 export default options
