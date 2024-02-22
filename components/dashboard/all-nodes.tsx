d81f52989fab7e663d9e6bf6660872263bd79b41
smart0x0 <tuanphamgolden@gmail.com>
2024-02-22 10:37:51 -0500

    feat: added api to get the nodes for user and showed blockchains count on node card

--- components/dashboard/all-nodes.tsx
+++ components/dashboard/all-nodes.tsx
@@ -11,12 +11,23 @@ import { Button } from "@/components/ui/button"
 import { Skeleton } from "@/components/ui/skeleton"
 import { FiCloud } from "react-icons/fi"
 import { useQuery } from "@tanstack/react-query"
+import { useMemo } from "react"
 
 export function CardNodesAll() {
   const { isPending, data } = useQuery<[]>({
     queryKey: ["node/list"],
     queryFn: () => fetch("/api/nodes/list").then((res) => res.json()),
   })
+
+  const blockchainCnt = useMemo(() => {
+    const blockchains: any[] = []
+    data?.forEach((node: any) => {
+      if (!blockchains.find(node.server.blockchain.id)) {
+        blockchains.push(node.server.blockchain.id)
+      }
+    })
+    return blockchains.length
+  }, [data])
 
   return (
     <Card>
@@ -35,7 +46,9 @@ export function CardNodesAll() {
         ) : (
           <>
             <div className="text-4xl font-bold">{data?.length}</div>
-            <p className="text-xs text-muted-foreground">on 5 blockchains</p>
+            <p className="text-xs text-muted-foreground">
+              on {blockchainCnt} blockchains
+            </p>
           </>
         )}
         <Button className="mt-6 w-full items-center">
