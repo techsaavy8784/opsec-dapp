200281bb7eb198be6c3a2247e8d9dcfc97176568
techsaavy8784 <blueskydev1205@gmail.com>
2024-02-27 09:05:28 -0700

    update node detail page

--- app/(user)/nodes/[id]/page.tsx
+++ app/(user)/nodes/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from "react"
+import React, { useEffect, useRef } from "react"
 import { useQuery } from "@tanstack/react-query"
 import { Skeleton } from "@/components/ui/skeleton"
 import { NodeType } from "../page"
@@ -14,10 +14,24 @@ interface NodeProps {
 }
 
 const Node: React.FC<NodeProps> = ({ params: { id } }) => {
-  const { isPending, data } = useQuery<NodeType>({
+  const timer = useRef<NodeJS.Timeout>()
+
+  const { isPending, data, refetch } = useQuery<NodeType>({
     queryKey: [`nodes/${id}`],
     queryFn: () => fetch(`/api/nodes/${id}`).then((res) => res.json()),
   })
+
+  useEffect(() => {
+    if (data?.status === "LIVE") {
+      clearInterval(timer.current)
+    } else {
+      timer.current = setInterval(() => {
+        refetch()
+      }, 10000)
+    }
+
+    return () => clearInterval(timer.current)
+  }, [data?.status, refetch])
 
   if (isPending || !data) {
     return (
@@ -25,93 +39,75 @@ const Node: React.FC<NodeProps> = ({ params: { id } }) => {
     )
   }
 
+  const daysPassed = Math.min(
+    Math.round(
+      (Date.now() - new Date(data.createdAt).getTime()) / (1000 * 3600 * 24),
+    ),
+    90,
+  )
+
   return (
-    <div className="space-y-4">
-      <div className="p-4 rounded-[20px] bg-[#18181B] space-y-2">
-        <div className="flex bg-[url(/image/node.png)] rounded-[16px] bg-center bg-cover bg-no-repeat h-[172px] justify-center items-center ">
+    <div className="space-y-2">
+      <Image
+        src={`/icons/blockchain/${data.server.blockchain.name
+          .toLowerCase()
+          .replace(/ /g, "-")}.png`}
+        alt=""
+        width={180}
+        height={180}
+        className="object-contain m-auto"
+      />
+      <h1 className="text-[#52525B]">{data.server.blockchain.description}</h1>
+      <div className="flex items-center justify-between pt-5">
+        <h1 className="text-[14px] font-[500] text-[#52525B]">Chain name</h1>
+        <h1 className="text-[14px] font-[500] text-[#fff]">
+          {data.server.blockchain.name}
+        </h1>
+      </div>
+      <div className="flex items-center justify-between">
+        <h1 className="text-[14px] font-[500] text-[#52525B]">Stats</h1>
+        <div className="flex items-center gap-1">
           <Image
-            src={`/icons/blockchain/${data.server.blockchain.name
-              .toLowerCase()
-              .replace(/ /g, "-")}.png`}
-            alt=""
-            width={180}
-            height={180}
-            className="object-contain m-auto"
+            src="/icons/tick-circle.svg"
+            alt="tick-circle"
+            width={18}
+            height={18}
           />
-        </div>
-        <h1 className="text-[#52525B]">{data.server.blockchain.description}</h1>
-        <div className="flex items-center justify-between">
-          <h1 className="text-[14px] font-[500] text-[#52525B]">Chain name</h1>
-          <Image
-            src="/icons/celestia-node.svg"
-            alt="icon"
-            width={20}
-            height={20}
-          />
-          {/* <h1 className="font-[600] text-[16px] text-[#fff]">
-            {data.server.blockchain.name}
-          </h1> */}
+          <h1 className="font-[600] text-[14px] text-[#10B981]">
+            {data.status.toUpperCase()}
+          </h1>
         </div>
       </div>
-      <div className="p-4 rounded-[20px] bg-[#18181B] space-y-2">
-        <div className="flex items-center justify-between pb-3 border-b border-[#52525B]">
-          <h1 className="font-[600] text-[16px] text-[#fff]">Nodes Stats</h1>
-          <div className="flex items-center gap-1">
-            <Image
-              src="/icons/tick-circle.svg"
-              alt="tick-circle"
-              width={18}
-              height={18}
-            />
-            <h1 className="font-[600] text-[14px] text-[#10B981]">ACTIVE</h1>
+
+      {data.status === "LIVE" && (
+        <>
+          <div className="flex items-center justify-between pt-3">
+            <h1 className="text-[14px] font-[500] text-[#52525B]">
+              Activated date
+            </h1>
+            <h1 className="text-[14px] font-[500] text-[#fff]">
+              {formatDate(data.createdAt)}
+            </h1>
           </div>
-        </div>
 
-        <div className="flex items-center justify-between">
-          <h1 className="text-[14px] font-[500] text-[#52525B]">
-            Total time validating
-          </h1>
-          <h1 className="text-[14px] font-[500] text-[#fff]">23 mins</h1>
-        </div>
-
-        <div className="flex items-center justify-between">
-          <h1 className="text-[14px] font-[500] text-[#52525B]">
-            Time last active
-          </h1>
-          <h1 className="text-[14px] font-[500] text-[#fff]">2d ago</h1>
-        </div>
-
-        <div className="flex items-center justify-between">
-          <h1 className="text-[14px] font-[500] text-[#52525B]">
-            Last rotation index
-          </h1>
-          <h1 className="text-[14px] font-[500] text-[#fff]">1.5 hrs</h1>
-        </div>
-
-        <div className="flex items-center justify-between">
-          <h1 className="text-[14px] font-[500] text-[#52525B]">
-            Activated date
-          </h1>
-          <h1 className="text-[14px] font-[500] text-[#fff]">2d ago</h1>
-        </div>
-
-        <div className="flex">
-          {new Array(100)
-            .fill(0)
-            ?.map((_, i) => (
-              <div
-                key={i}
-                className={`w-[1%] h-[48px] m-[1px] ${
-                  i === 24 || i === 25 ? "bg-[#F87171]" : "bg-[#10B981] "
-                } rounded-[3px]`}
-              />
-            ))}
-        </div>
-        <div className="flex items-center justify-between">
-          <h1 className="text-[14px] font-[500] text-[#52525B]">90 days ago</h1>
-          <h1 className="text-[14px] font-[500] text-[#fff]">Today</h1>
-        </div>
-      </div>
+          <div className="flex">
+            {new Array(daysPassed)
+              .fill(0)
+              ?.map((_, i) => (
+                <div
+                  key={i}
+                  className={`w-[1%] h-[48px] m-[1px] bg-[#10B981] rounded-[3px]`}
+                />
+              ))}
+          </div>
+          <div className="flex items-center justify-between">
+            <h1 className="text-[14px] font-[500] text-[#52525B]">
+              {daysPassed} days ago
+            </h1>
+            <h1 className="text-[14px] font-[500] text-[#52525B]">Today</h1>
+          </div>
+        </>
+      )}
     </div>
   )
 }
