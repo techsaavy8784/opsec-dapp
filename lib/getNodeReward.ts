1bfa7b7304a98bd4c53e4a3abf0c9d5ebc6bc569
smart0x0 <tuanphamgolden@gmail.com>
2024-05-14 02:46:56 -0400

    fix: updated reward calculation

--- lib/getNodeReward.ts
+++ lib/getNodeReward.ts
@@ -1,5 +1,6 @@
 import dayjs from "dayjs"
 import prisma from "@/prisma"
+import { PAY_TYPE } from "@prisma/client"
 
 const getNodeReward = async (userId: number, nodeId: number) => {
   const now = dayjs()
@@ -20,7 +21,7 @@ const getNodeReward = async (userId: number, nodeId: number) => {
         id: nodeId,
         NOT: { server: null },
       },
-      include: { blockchain: true },
+      include: { blockchain: true, payments: true },
     }),
 
     prisma.reward
@@ -45,18 +46,46 @@ const getNodeReward = async (userId: number, nodeId: number) => {
   const ownership = paidCredit / node.blockchain.price
 
   if (now.isBefore(lockTime)) {
-    return { reward: 0, ownership }
+    return {
+      reward: 0,
+      ownership: node.blockchain.payType === PAY_TYPE.FULL ? 1 : ownership,
+    }
   }
 
-  let rewardPeriod = now.diff(purchaseTime, "month")
+  if (node.blockchain.payType === PAY_TYPE.FULL) {
+    const expiration = node.payments.reduce(
+      (total, item) => (total += item.duration),
+      0,
+    )
+    let expirationTime = dayjs(node.createdAt).add(expiration, "day")
+    if (now.isBefore(expirationTime)) {
+      expirationTime = now
+    }
 
-  if (withdrawTime) {
-    rewardPeriod -= withdrawTime.diff(purchaseTime, "month")
-  }
+    if (withdrawTime?.isAfter(expirationTime)) {
+      return { reward: 0, ownership: 1 }
+    } else {
+      let rewardPeriod = expirationTime.diff(purchaseTime, "month")
+      if (withdrawTime) {
+        rewardPeriod -= withdrawTime.diff(purchaseTime, "month")
+      }
 
-  return {
-    reward: (node.blockchain.rewardPerMonth ?? 0) * rewardPeriod * ownership,
-    ownership,
+      return {
+        reward: (node.blockchain.rewardPerMonth ?? 0) * rewardPeriod,
+        ownership: 1,
+      }
+    }
+  } else {
+    let rewardPeriod = now.diff(purchaseTime, "month")
+
+    if (withdrawTime) {
+      rewardPeriod -= withdrawTime.diff(purchaseTime, "month")
+    }
+
+    return {
+      reward: (node.blockchain.rewardPerMonth ?? 0) * rewardPeriod * ownership,
+      ownership,
+    }
   }
 }
 
