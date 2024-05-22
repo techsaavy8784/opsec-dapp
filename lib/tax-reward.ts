2e80122b6a3ebc7afc013133a3855eb3191152d9
techsaavy8784 <blueskydev1205@gmail.com>
2024-05-22 06:45:58 -0600

    update

--- lib/tax-reward.ts
+++ lib/tax-reward.ts
@@ -5,6 +5,8 @@ import prisma from "@/prisma"
 import covalentClient from "./covalent-client"
 import { publicClient } from "@/contract/client"
 
+const OPSEC_DECIMALS = 18
+
 const getTaxReward = async (userId: number, userAddress: string) => {
   const reward = await prisma.reward.findFirst({
     where: { userId: userId },
@@ -30,18 +32,21 @@ const getTaxReward = async (userId: number, userAddress: string) => {
       userAddress,
       { days: 10 },
     )
-  const opsecData = balanceHistory.items.find(
+
+  const recentOpsecBalanceHistory = balanceHistory.items.find(
     (item) =>
       item.contract_address.toLowerCase() ===
       process.env.NEXT_PUBLIC_OPSEC_TOKEN_ADDRESS,
   )
-  if (!opsecData) {
+
+  if (!recentOpsecBalanceHistory) {
     return 0
   }
-  for (let i = 0; i < opsecData.holdings.length; i++) {
+
+  for (let i = 0; i < recentOpsecBalanceHistory.holdings.length; i++) {
     if (
-      BigInt(opsecData.holdings[i].open.balance ?? "0") <
-      BigInt(opsecData.holdings[i + 1].open.balance ?? "0")
+      BigInt(recentOpsecBalanceHistory.holdings[i].open.balance ?? 0) <
+      BigInt(recentOpsecBalanceHistory.holdings[i + 1].open.balance ?? 0)
     ) {
       return 0
     }
@@ -58,44 +63,45 @@ const getTaxReward = async (userId: number, userAddress: string) => {
       item.contract_address.toLowerCase() ===
       process.env.NEXT_PUBLIC_OPSEC_TOKEN_ADDRESS,
   )
+
   if (!latestOpsec) {
     return 0
   }
 
-  const latestDate = dayjs(
+  const fromDate = dayjs(
     Math.max(
       dayjs(latestOpsec.last_transferred_at).add(10, "day").unix(),
       reward ? dayjs(reward.rewardWithdrawTime).unix() : 0,
     ),
   )
 
-  const [ethbalance, balance] = await Promise.all([
-    publicClient.getBalance({
-      address: process.env.NEXT_PUBLIC_STAKING_CONTRACT as `0x${string}`,
-    }),
-    publicClient.readContract({
-      abi: erc20Abi,
-      address: process.env.NEXT_PUBLIC_OPSEC_TOKEN_ADDRESS as `0x${string}`,
-      functionName: "balanceOf",
-      args: [userAddress as `0x${string}`],
+  const [stakingBalance, userBalance, lastTax] = await Promise.all([
+    publicClient
+      .getBalance({
+        address: process.env.NEXT_PUBLIC_STAKING_CONTRACT as `0x${string}`,
+      })
+      .then((value) => Number(formatUnits(value, OPSEC_DECIMALS))),
+
+    publicClient
+      .readContract({
+        abi: erc20Abi,
+        address: process.env.NEXT_PUBLIC_OPSEC_TOKEN_ADDRESS as `0x${string}`,
+        functionName: "balanceOf",
+        args: [userAddress as `0x${string}`],
+      })
+      .then((value) => Number(formatUnits(value, OPSEC_DECIMALS))),
+
+    prisma.taxHistory.findFirst({
+      where: { createdAt: { lte: fromDate.toDate() } },
+      orderBy: { createdAt: "desc" },
     }),
   ])
-  const balanceOpsec = Number(formatUnits(balance, 18))
 
-  const latestTax = await prisma.taxHistory.findFirst({
-    where: { createdAt: { lte: latestDate.toDate() } },
-    orderBy: { createdAt: "desc" },
-  })
+  const taxAmount = stakingBalance - (lastTax?.amount ?? 0)
 
-  const taxAmount =
-    Number(formatUnits(ethbalance, 18)) - (latestTax?.amount || 0)
+  const percent = Number(process.env.NEXT_PUBLIC_TAX_PERCENT) / 100
 
-  const taxReward =
-    (taxAmount *
-      balanceOpsec *
-      Number(process.env.NEXT_PUBLIC_TAX_PERCENT as string)) /
-    taxHistory.totalOpsec /
-    100
+  const taxReward = (percent * taxAmount * userBalance) / taxHistory.totalOpsec
 
   return Number(taxReward)
 }
