64f6a824f01805a96b63affd8b0b06080b229c4c
techsaavy8784 <blueskydev1205@gmail.com>
2024-06-13 02:53:09 -0600

    add PRODUCTION env variable

--- contract/client.ts
+++ contract/client.ts
@@ -2,6 +2,6 @@ import { createPublicClient, http } from "viem"
 import { mainnet, sepolia } from "viem/chains"
 
 export const publicClient = createPublicClient({
-  chain: process.env.NODE_ENV === "production" ? mainnet : sepolia,
+  chain: process.env.NODE_ENV === "production" && Number(process.env.PROUDUCTION) ? mainnet : sepolia,
   transport: http(process.env.RPC_URL),
 })
