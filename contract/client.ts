3ebcb74392bbff5ee23773e7a8aec929718bd8aa
techsaavy8784 <blueskydev1205@gmail.com>
2024-06-13 03:45:30 -0600

    update

--- contract/client.ts
+++ contract/client.ts
@@ -2,6 +2,6 @@ import { createPublicClient, http } from "viem"
 import { mainnet, sepolia } from "viem/chains"
 
 export const publicClient = createPublicClient({
-  chain: process.env.NODE_ENV === "production" && Number(process.env.PROUDUCTION) ? mainnet : sepolia,
+  chain: process.env.NODE_ENV === "production" && Number(process.env.PRODUCTION) ? mainnet : sepolia,
   transport: http(process.env.RPC_URL),
 })
